a95c390a162c70e3f4d312256466da54e693ddfa
zied fakhfakh <fakhfakh.zied@gmail.com>
2018-06-18 11:12:45 +0200

    lista prodotti

---
+++ js/lista_prodotti.js
@@ -0,0 +1,42 @@
+// lista_telefonia
+var lista_telefonia = [{
+    id: 1,
+    nome: 'Huawei',
+    prezzo: 200,
+    categoria: 'telefonia',
+    immmagine: ''
+}, {
+    id: 2,
+    nome: 'Samsung',
+    prezzo: 300,
+    categoria: 'telefonia',
+    immmagine: ''
+}, {
+    id: 3,
+    nome: 'Iphone',
+    prezzo: 350,
+    categoria: 'telefonia',
+    immmagine: ''
+}];
+
+//lista_computer
+var lista_computer = [{
+    id: 1,
+    nome: 'Asus',
+    prezzo: 600,
+    categoria: 'computer',
+    immmagine: ''
+}, {
+    id: 2,
+    nome: 'Samsung',
+    prezzo: 700,
+    categoria: 'computer',
+    immmagine: ''
+}, {
+    id: 3,
+    nome: 'Mac',
+    prezzo: 1000,
+    categoria: 'computer',
+    immmagine: ''
+}];
+
